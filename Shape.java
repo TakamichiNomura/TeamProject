fe5cadbcf75e071718d9eb87c2c24b2d15765b77
KeiLee <klee3309@icloud.com>
2019-09-04 15:05:15 +0900

    //消した

--- Shape.java
+++ Shape.java
@@ -6,10 +6,10 @@
  * @version (20190902)
  */
 interface Shape{ 
-    final double PI = 3.14; // 상수
-    void draw(); // 도형을 그리는 추상 메소드
-    double getArea(); // 도형의 면적을 리턴하는 추상 메소드
-    default public void redraw(){ // 디폴트 메소드
+    final double PI = 3.14; 
+    void draw(); 
+    double getArea(); 
+    default public void redraw(){ 
         System.out.print("--- 다시 그립니다. "); 
         draw(); 
     }
